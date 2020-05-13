308575608ce322cb9fbc1c6add52cf2610759ff2
Basant Bhandari <36443209+basantbhandari@users.noreply.github.com>
2020-05-13 22:10:12 +0545

    initial commit

---
+++ Assets/Scripts/AccelerometerMove.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AccelerometerMove : MonoBehaviour { 
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //taking accelero meter input
+        float temp = Input.acceleration.x;
+        Debug.Log(temp);
+        transform.Translate(0, temp, 0);
+
+
+    }
+}
---
+++ Assets/Scripts/GameOver.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOver : MonoBehaviour
+{
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("GameOver in side trigger");
+
+        //for just delay
+        for (int i = 0; i<=500000000; i++) ;
+
+
+        SceneManager.LoadScene("Main");
+
+    }
+
+}
